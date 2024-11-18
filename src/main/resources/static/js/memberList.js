a27a0d2d7223ea8a509658a69945ce1479aeec67
Kristofferjoha <kristofferjoha@gmail.com>
2024-11-18 10:43:57 +0100

    f
    f

--- src/main/resources/static/js/memberList.js
+++ src/main/resources/static/js/memberList.js
@@ -1,37 +1,38 @@
+import { fetchApprovedMembers, fetchBoats, fetchBerth } from "./memberFetch.js";
 
+async function initialize() {
+    const members = await fetchApprovedMembers();
+    console.log(members);
+    const boats = await fetchBoats();
+    console.log(boats);
+    const berths = await fetchBerth();
+    console.log(berths);
 
-import {fetchApprovedMembers, fetchBoats, fetchBerth} from "./memberFetch.js";
+    // Now call calculateAreal() after fetching the data
+    calculateAreal(boats); // Pass boats data to the function
+    getMemberList(members, boats, berths); // Pass all the required data
+}
 
-const members = await fetchApprovedMembers();
-console.log(members);
-const boats = await fetchBoats();
-console.log(boats);
-const berths = await fetchBerth();
-console.log(berths);
+initialize();
 
-function calculateAreal(){
+function calculateAreal(boats){
     boats.forEach(boat => {
         boat.areal = boat.length * boat.width;
-        console.log("boats berth ID:" + boat.berthID + "boat areal:" + boat.areal);
-    })
+        console.log("Boat ID:" + boat.boatID + " Boat areal: " + boat.areal);
+    });
 }
 
-calculateAreal();
-
-function convertKeys(){
+function convertKeys(members, boats, berths){
     berths.forEach(berth => {
         console.log(Object.keys(berth));
-    })
+    });
     boats.forEach(boat => {
         console.log(Object.keys(boat));
-    })
+    });
     members.forEach(member => {
         console.log(Object.keys(member));
-    })
+    });
 }
-
-convertKeys();
-
 
 function addCells(tr, data){
     // Iterate over the data
@@ -41,37 +42,37 @@ function addCells(tr, data){
     });
 }
 
-function getMemberList(){
+function getMemberList(members, boats, berths){
     const table = document.getElementById("memberListBody");
-    // For each berth, find corresponding boat and member
+    // For each member, find corresponding boat and berth
     members.forEach(member => {
         member.correspondingBoat = boats.find(boat => boat.memberID === member.memberID);
-        console.log(JSON.stringify(member.correspondingBoat));
+        console.log("Corresponding Boat for Member:", JSON.stringify(member.correspondingBoat));
+
         if (member.correspondingBoat){
-            console.log(JSON.stringify(member.correspondingBoat.berthID));
-            member.correspondingBerth = berths.find(berth => berth.berth.berthID === member.correspondingBoat.berthID);
-            console.log(JSON.stringify(member.correspondingBerth));
-            if (member.correspondingBerth){
-                console.log(JSON.stringify(member.correspondingBerth.berth.berthID));
-            }
+            console.log("Boat's Berth ID:", member.correspondingBoat.berthID);
+            member.correspondingBerth = berths.find(berth => berth.berthID === member.correspondingBoat.berthID);
+            console.log("Corresponding Berth:", JSON.stringify(member.correspondingBerth));
         }
     });
-    //For each berth, create a row and add the data
+
+    // For each member, create a row and add the data
     members.forEach(member => {
+        console.log(member)
         var row = table.insertRow();
-        addCells(row, [member.memberID, member.name, member.address, member.email, member.phonenumber]);
+        addCells(row, [member.memberID, member.name, member.address, member.email, member.phonenumber,"","","","","",""]);
+
         // find boat assigned to berth and corresponding member
-        if (member.correspondingBoat ){
-                addCells(row, [member.correspondingBoat.boat.name,member.correspondingBoat.boat.length + "m", member.correspondingBoat.boat.width + "m", member.correspondingBoat.boat.areal + "m^2", "hey"])
-            if (member.correspondingBerth){
-                addCells(row, [member.correspondingBerth.berth.name,]);
-            }
-        } else {
-            addCells(row, ["", "", "", "", "", ""]);
-        }
+
     });
 }
 
-getMemberList();
-
-//chatgpt was here
+/*        if (member.correspondingBoat){
+            // Since correspondingBoat is the boat object, access its properties directly
+            addCells(row, [member.correspondingBoat.name, member.correspondingBoat.length + "m", member.correspondingBoat.width + "m", member.correspondingBoat.areal + "m^2"]);
+            if (member.correspondingBerth){
+                addCells(row, [member.correspondingBerth.name]);
+            }
+        } else {
+            addCells(row, ["", "", "", "", "", ""]);
+        }*/
