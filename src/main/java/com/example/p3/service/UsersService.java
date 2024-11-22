315b3752a743bc6078b8748dd4c5b937462596fa
3xTremee <lasse.b.soerensen@gmail.com>
2024-11-22 14:49:16 +0100

    fikset så man kan opdaterer kodeord
    jeg mangler dog at tilføje knappen i de sidste html filer

--- src/main/java/com/example/p3/service/UsersService.java
+++ src/main/java/com/example/p3/service/UsersService.java
@@ -3,10 +3,12 @@ package com.example.p3.service;
 import com.example.p3.model.Users;
 import com.example.p3.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -18,6 +20,9 @@ public class UsersService implements UserDetailsService {
 
     @Autowired
     private UsersRepository usersRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -39,5 +44,20 @@ public class UsersService implements UserDetailsService {
         } else {
             throw new UsernameNotFoundException(username);
         }
+    }
+
+    public void updatePassword(String username, String currentPassword, String newPassword) throws Exception{
+        // finder users by username for at finde den rigteige user.
+        Users user = usersRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        // man tjekker om den currentPassword som er indtastet matcher det nuværende password
+        if (!passwordEncoder.matches(currentPassword, user.getPassword())) {
+            throw new Exception("Passwords do not match");
+        }
+
+        // encode, set og save det nye password
+        user.setPassword(passwordEncoder.encode(newPassword));
+        usersRepository.save(user);
     }
 }
